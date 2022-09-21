fbb43fe7f68688bfc47a92adb96d3905e9d7734f
Mikel Arrieta Etxaniz <ir.mikel.arrieta.etxaniz@gmail.com>
2022-09-21 10:36:16 +0200

    JWT implemented

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,15 +3,19 @@ import { PrismaService } from "src/prisma/prisma.service";
 import { AuthDto } from "./dto";
 import * as argon from "argon2";
 import { PrismaClientKnownRequestError } from "@prisma/client/runtime";
+import { JwtService } from "@nestjs/jwt";
+import { ConfigService } from "@nestjs/config";
 @Injectable({})
 export class AuthService {
-   constructor(private prisma: PrismaService) {}
+   constructor(
+      private prisma: PrismaService,
+      private jwt: JwtService,
+      private config: ConfigService,
+   ) {}
 
    async signup(dto: AuthDto) {
-      // generate the password hash
       const hash = await argon.hash(dto.password);
 
-      // save the new user in db
       try {
          const user = await this.prisma.user.create({
             data: {
@@ -20,10 +24,7 @@ export class AuthService {
             },
          });
 
-         delete user.hash;
-
-         // return the saved user
-         return user;
+         return this.signToken(user.id, user.email);
       } catch (error) {
          if (error instanceof PrismaClientKnownRequestError) {
             if ((error.code = "P2002")) {
@@ -34,28 +35,33 @@ export class AuthService {
    }
 
    async signin(dto: AuthDto) {
-      // find the user by email
       const user = await this.prisma.user.findUnique({
          where: {
             email: dto.email,
          } as any,
       });
-      // if user does not exist throw exception
-      if (!user) {
-         throw new ForbiddenException("Credentials incorrect.");
-      }
 
-      // compare password
+      if (!user) throw new ForbiddenException("Credentials incorrect.");
+
       const pwMatches = await argon.verify(user.hash, dto.password);
 
-      // if password incorrect throw exception
-      if (!pwMatches) {
-         throw new ForbiddenException("Credentials incorrect.");
-      }
+      if (!pwMatches) throw new ForbiddenException("Credentials incorrect.");
 
-      delete user.hash;
+      return this.signToken(user.id, user.email);
+   }
 
-      // send back the user
-      return user;
+   async signToken(
+      userId: number,
+      email: string,
+   ): Promise<{ access_token: string }> {
+      const token = await this.jwt.signAsync(
+         {
+            sub: userId,
+            email: email,
+         },
+         { expiresIn: "15m", secret: this.config.get("JWT_SECRET") },
+      );
+
+      return { access_token: token };
    }
 }
