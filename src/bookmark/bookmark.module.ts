6e3fff658a2b01841351e5691a33708ca81b0f51
Mikel Arrieta Etxaniz <ir.mikel.arrieta.etxaniz@gmail.com>
2022-09-27 09:52:55 +0200

    Linter fixes

--- src/bookmark/bookmark.module.ts
+++ src/bookmark/bookmark.module.ts
@@ -1,9 +1,9 @@
 import { Module } from "@nestjs/common";
-import { BookmarkController } from './bookmark.controller';
-import { BookmarkService } from './bookmark.service';
+import { BookmarkController } from "./bookmark.controller";
+import { BookmarkService } from "./bookmark.service";
 
 @Module({
-  controllers: [BookmarkController],
-  providers: [BookmarkService]
+   controllers: [BookmarkController],
+   providers: [BookmarkService],
 })
 export class BookmarkModule {}
